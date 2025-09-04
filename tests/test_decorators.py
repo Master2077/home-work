21db2d58eb5ac5f473e593b83cbe96ff0339773d
Stoun <doctor_stoun@example.com>
2025-09-04 21:49:14 +0300

    test_decorators

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,1 +1,16 @@
+import pytest
+import time
+from scr.decorators import my_function
 
+def test_my_function_zero_division(capsys):
+    # Тестируем деление на 0
+    my_function(10, 0)
+
+    # Захватываем вывод
+    captured = capsys.readouterr()
+
+    # Проверяем, что вывод содержит ожидаемые строки
+    assert "Функция: my_function" in captured.out
+    assert "Ошибка: division by zero" in captured.out
+    assert "Входные параметры:" in captured.out
+    assert "Время выполнения:" in captured.out
