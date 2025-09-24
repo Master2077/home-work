85566dddbdb89321c6565d62cd014f720842438e
Stoun <doctor_stoun@example.com>
2025-09-24 19:39:01 +0300

    update_load_operations

--- scr/utils.py
+++ scr/utils.py
@@ -2,11 +2,8 @@ import json
 import os
 
 def load_operations(filename='operations.json'):
-    base_dir = os.path.dirname(__file__)              # utils/
-    data_path = os.path.join(base_dir, '..', 'data', filename)
-    data_path = os.path.normpath(data_path)          # корректирует ../
     try:
-        with open(data_path, encoding='utf-8') as f:
+        with open('../data/operations.json', encoding='utf-8') as f:
             data = json.load(f)
         if not isinstance(data, list):
             return []
