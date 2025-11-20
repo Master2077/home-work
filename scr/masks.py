0af8ac1455f6b3af5cb070dfe44ba38c3f5977fc
Stoun <doctor_stoun@example.com>
2025-11-20 21:19:50 +0300

    логирование utils и masks

--- scr/masks.py
+++ scr/masks.py
@@ -1,3 +1,13 @@
+import logging
+from multiprocessing.util import DEBUG
+
+logging.basicConfig(
+    level=DEBUG,
+    format="%(asctime)s - %(filename)s - %(levelname)s: %(message)s",
+    handlers=[logging.FileHandler("../logs/masks.log", "w", encoding="utf-8")],
+)
+
+
 def get_mask_card_number(card_number: str) -> str:
     """
     Принимает номер карты в виде строки, проверяет его корректность и маскирует внутреннюю часть.
@@ -5,9 +15,11 @@ def get_mask_card_number(card_number: str) -> str:
     Функция заменяет символы номера карты, начиная с 11-го символа до символа перед последними 4 символами,
     на символ '*'. Первые 10 символов и последние 4 остаются без изменений.
     """
+    logging.debug("Проверяем все ли символы карты - цифры, и длина карты больше 14 ")
     if card_number.isdigit() and len(card_number) > 14:
 
         # Преобразуем строку в список символов для возможности изменения элементов
+        logging.debug("Маскируем номер карты")
         card_number_list: list[str] = list(card_number)
 
         # Заменяем символы с 10-го до предпоследних 4 на '*'
@@ -15,9 +27,11 @@ def get_mask_card_number(card_number: str) -> str:
             card_number_list[i] = "*"
 
         # Объединяем список обратно в строку
+        logging.info("Получаем маскированный номер карты")
         return "".join(card_number_list)
 
     else:
+        logging.warning("Неправильный номер карты")
         return "Ошибка: Неправильный номер карты"
 
 
@@ -28,9 +42,11 @@ def get_mask_account(mask_account: str) -> str:
     Функция заменяет символы номера карты, начиная с 0-го символа до символа перед последними 4 символами,
     на символ '*'. Последние 4 символа  остаются без изменений.
     """
+    logging.debug("Проверяем все ли символы счета - цифры, и длина счета больше 5")
     if mask_account.isdigit() and len(mask_account) > 5:
 
         # Преобразуем строку в список символов для возможности изменения элементов
+        logging.debug("Маскируем номер счета")
         mask_account_list: list[str] = list(mask_account)
 
         # Заменяем символы с 0-го до предпоследних 4 на '*'
@@ -38,16 +54,20 @@ def get_mask_account(mask_account: str) -> str:
             mask_account_list[i] = "*"
 
         # Объединяем список обратно в строку
+        logging.info("Получаем масикрованный номер счета")
         return "".join(mask_account_list)
 
     else:
+        logging.warning("Неправильный номер счета")
         return "Ошибка: Неправильный номер счета"
 
 
 if __name__ == "__main__":
+    logging.debug("Вводим номер карты")
     card_number: str = input("Введите номер карты: ")
     masked_number: str = get_mask_card_number(card_number)
 
+    logging.debug("Вводим номер счета")
     mask_account: str = input("Введите номер счета: ")
     masked_account: str = get_mask_account(mask_account)
 
--- scr/utils.py
+++ scr/utils.py
@@ -1,4 +1,20 @@
 import json
+import logging
+
+success_logger = logging.getLogger("success_utils")
+success_logger.setLevel(logging.DEBUG)
+file_handler1 = logging.FileHandler("../logs/success_utils.log", "w", encoding="utf-8")
+file_formatter1 = logging.Formatter("%(asctime)s - %(filename)s - %(levelname)s: %(message)s")
+file_handler1.setFormatter(file_formatter1)
+success_logger.addHandler(file_handler1)
+
+errors_logger = logging.getLogger("errors_utils")
+errors_logger.setLevel(logging.ERROR)
+file_handler2 = logging.FileHandler("../logs/errors_utils.log", "w", encoding="utf-8")
+file_formatter2 = logging.Formatter("%(asctime)s - %(filename)s - %(levelname)s: %(message)s")
+file_handler2.setFormatter(file_formatter2)
+errors_logger.addHandler(file_handler2)
+
 
 # Переменная с указанием пути до json файла
 path = "../data/operations.json"
@@ -7,17 +23,20 @@ path = "../data/operations.json"
 def load_operations(path):
     try:
         # Читаем данные в operations.json
-        with open(path, encoding='utf-8') as f:
+        success_logger.debug(f"Читаем данные в {path}")
+        with open(path, encoding="utf-8") as f:
             data = json.load(f)
         # Если в operations.json нет списка, то возвращаем пустой список
         if not isinstance(data, list):
+            success_logger.info(f"Получаем пустой список из {path}")
             return []
         # Возвращаем данные из operations.json
+        success_logger.info(f"Возвращаем данные из {path}")
         return data
     # Если файл operations.json не был обнаружен, то возвращаем пустой список
     except (FileNotFoundError, json.JSONDecodeError):
+        errors_logger.error(f"Файл {path} не обнаружен")
         return []
 
 
 result_load_operations = load_operations(path)
-
